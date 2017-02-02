ec7cba7467cbac4e71371df233c8ddb29d807255
Kazuyuki Ishijima <k.ishijima18@gmail.com>
2017-02-02 04:03:46 +0000

    dive09

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
+
 n = 1
 while n <= 100
   Blog.create(
