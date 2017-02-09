7f7c50222be8b636edb59687ceebef4316e967ac
kazuyuki <k,ishijima18@gmail.com>
2017-02-09 23:07:43 +0900

    sns

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # SNSログインから取得してきた画像を表示させる
   def profile_img(user)
-    return image_tag(user.avatar, alt: user.name) if user.avatar.present?
+    return image_tag(user.avatar, alt: user.name) if user.avatar?
 
     unless user.provider.blank?
       img_url = user.image_url
